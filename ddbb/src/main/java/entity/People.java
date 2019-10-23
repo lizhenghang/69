79c32b4ede8b3051b4be928c0306556563e593c7
lizhenghang <1475546247@qq.com>
2019-10-23 15:05:31 +0800

    新建项目：ddbb

---
+++ ddbb/src/main/java/entity/People.java
@@ -0,0 +1,12 @@
+package entity;
+
+/**
+ * 游戏人类
+ */
+public class People {
+
+    private String name;
+    private String sex;
+    private int age;
+
+}
---
+++ ddbb/src/main/java/entity/Player.java
@@ -0,0 +1,14 @@
+package entity;
+
+/**
+ * 玩家实体
+ */
+public class Player extends People{
+
+    private String id;      //
+    private String username;
+    private String password;
+    private int level;
+
+
+}
