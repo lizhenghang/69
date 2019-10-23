789608cf16b0d19e171cc0e318fc76f15527599d
lizhenghang <1475546247@qq.com>
2019-10-23 17:30:44 +0800

    新增玩家类，人类，以及坐标excel表

--- ddbb/src/main/java/entity/People.java
+++ ddbb/src/main/java/entity/People.java
@@ -5,8 +5,31 @@ package entity;
  */
 public class People {
 
-    private String name;
-    private String sex;
-    private int age;
+    private String name;        //姓名
+    private String sex;         //性别
+    private int age;            //年龄
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- ddbb/src/main/java/entity/Player.java
+++ ddbb/src/main/java/entity/Player.java
@@ -5,10 +5,103 @@ package entity;
  */
 public class Player extends People{
 
-    private String id;      //
-    private String username;
-    private String password;
-    private int level;
+    private String id;
+    private String username;        //用户名
+    private String password;        //密码
+    private double money;           //充值钱币
+    private int level;              //等级
+    private String tel;             //电话
+    private String qq;              //QQ
+    private String wechat;          //微信
+    private String alipay;          //支付宝
+    private String iCard;           //身份证号
+    private String email;           //邮箱
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat;
+    }
+
+    public String getAlipay() {
+        return alipay;
+    }
+
+    public void setAlipay(String alipay) {
+        this.alipay = alipay;
+    }
+
+    public String getiCard() {
+        return iCard;
+    }
+
+    public void setiCard(String iCard) {
+        this.iCard = iCard;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
